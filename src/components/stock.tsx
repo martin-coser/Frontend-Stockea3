7eb56dfac556b899d8b4474a872b58368fc168fc
martin-coser <cosermartin@hotmail.com>
2025-05-22 11:14:38 -0300

    stockConScroll

--- src/components/stock.tsx
+++ src/components/stock.tsx
@@ -76,36 +76,38 @@ const Stock: React.FC = () => {
             className="w-1/2 p-1.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-500 transition"
           />
         </div>
-
-        <table className="w-full border-collapse rounded-lg overflow-hidden shadow-md border border-indigo-200 bg-gray-100">
-          <thead>
-            <tr className="bg-indigo-100">
-              <th className="border px-4 py-2 text-left text-sm font-semibold">Nombre</th>
-              <th className="border px-4 py-2 text-left text-sm font-semibold">Código</th>
-              <th className="border px-4 py-2 text-left text-sm font-semibold">Stock</th>
-              <th className="border px-4 py-2 text-left text-sm font-semibold">Descripción</th>
-              <th className="border px-4 py-2 text-left text-sm font-semibold">Marca</th>
-              <th className="border px-4 py-2 text-left text-sm font-semibold">Categoría</th>
-              <th className="border px-4 py-2 text-left text-sm font-semibold">Proveedor</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productos.map((producto: any) => (
-              <tr
-                key={producto.id}
-                className="hover:bg-gray-200"
-              >
-                <td className="border px-4 py-2 text-sm">{producto.nombre}</td>
-                <td className="border px-4 py-2 text-sm">{producto.codigo}</td>
-                <td className="border px-4 py-2 text-sm">{producto.stock}</td>
-                <td className="border px-4 py-2 text-sm">{producto.descripcion}</td>
-                <td className="border px-4 py-2 text-sm">{producto.marca.nombre}</td>
-                <td className="border px-4 py-2 text-sm">{producto.categoria.nombre}</td>
-                <td className="border px-4 py-2 text-sm">{producto.proveedor.nombre}</td>
+        <div className="max-h-[75vh] overflow-y-auto">
+          <table className="w-full border-collapse rounded-lg overflow-hidden shadow-md border border-indigo-200 bg-gray-100">
+            <thead>
+              <tr className="bg-indigo-100">
+                <th className="border px-4 py-2 text-left text-sm font-semibold">Nombre</th>
+                <th className="border px-4 py-2 text-left text-sm font-semibold">Código</th>
+                <th className="border px-4 py-2 text-left text-sm font-semibold">Stock</th>
+                <th className="border px-4 py-2 text-left text-sm font-semibold">Descripción</th>
+                <th className="border px-4 py-2 text-left text-sm font-semibold">Marca</th>
+                <th className="border px-4 py-2 text-left text-sm font-semibold">Categoría</th>
+                <th className="border px-4 py-2 text-left text-sm font-semibold">Proveedor</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {productos.map((producto: any) => (
+                <tr
+                  key={producto.id}
+                  className="hover:bg-gray-200"
+                >
+                  <td className="border px-4 py-2 text-sm">{producto.nombre}</td>
+                  <td className="border px-4 py-2 text-sm">{producto.codigo}</td>
+                  <td className="border px-4 py-2 text-sm">{producto.stock}</td>
+                  <td className="border px-4 py-2 text-sm">{producto.descripcion}</td>
+                  <td className="border px-4 py-2 text-sm">{producto.marca.nombre}</td>
+                  <td className="border px-4 py-2 text-sm">{producto.categoria.nombre}</td>
+                  <td className="border px-4 py-2 text-sm">{producto.proveedor.nombre}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
       </motion.div>
     </motion.div>
   );
